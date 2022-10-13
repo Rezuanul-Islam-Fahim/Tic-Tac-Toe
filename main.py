4fa2a960530783ecc0a010abb4e0e65576292fe4
Rezuanul Islam Fahim <rifahim98@gmail.com>
2022-10-13 23:03:58 +0600

    Create user position validation

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-board_items = ['', '', '', '', '', '', '', '', '']
+board_items = ['.', '.', '.', '.', '.', '.', '.', '.', '.']
 game_running = True
 
 
 # =============================
 # Function for printing the board
-def print_board():
+def print_board(board_items):
     print(board_items[6] + '|' + board_items[7] + '|' + board_items[8])
     print(board_items[3] + '|' + board_items[4] + '|' + board_items[5])
     print(board_items[0] + '|' + board_items[1] + '|' + board_items[2])
@@ -27,9 +27,38 @@ def choose_character_option():
         return ('0', 'X')
 
 
+# ==========================
+# Function for adding char to a position
+def update_board():
+    pass
+
+
+# ===========================
+# Function for validating position
+def validate_get_position():
+    pos = -1
+
+    while pos not in range(1, 10):
+        try:
+            pos = int(input('Enter a position: '))
+        except:
+            print('Please enter a valid input number between (1 - 9)')
+            pos = None
+
+        if not pos == None and pos not in range(1, 10):
+            print('Please enter a position between (1 - 9)')
+
+    return pos
+
+
 # ===============================
 # Main operation starts from here
 while game_running:
-    print_board()
+    print_board(board_items)
     player1, player2 = choose_character_option()
-    print(f'Your character: {player1}')
+    print(f'\n======= You have choosen: {player1} ========')
+    print('Your turn first\n')
+    board_ongoing = True
+
+    while board_ongoing:
+        pos = validate_get_position()
