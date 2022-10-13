85d9c3d3ff10d39cd367327532270f692c20440e
Rezuanul Islam Fahim <rifahim98@gmail.com>
2022-10-13 22:28:53 +0600

    Create empty board

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-board_items = ['x', 'o', 'x', 'o', 'x', 'o', 'o', 'x', 'x']
+board_items = ['', '', '', '', '', '', '', '', '']
 
 
+# Function for printing the board
 def print_board():
     print(board_items[6] + '|' + board_items[7] + '|' + board_items[8])
     print(board_items[3] + '|' + board_items[4] + '|' + board_items[5])
     print(board_items[0] + '|' + board_items[1] + '|' + board_items[2])
 
 
+# Main operation starts from here
 print_board()
