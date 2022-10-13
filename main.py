496e6a25ea3e95026ea82d3bcf6d4ee8e8c0fb56
Rezuanul Islam Fahim <rifahim98@gmail.com>
2022-10-13 22:40:24 +0600

    Choose one of the two characters

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 board_items = ['', '', '', '', '', '', '', '', '']
+game_running = True
 
 
+# =============================
 # Function for printing the board
 def print_board():
     print(board_items[6] + '|' + board_items[7] + '|' + board_items[8])
@@ -8,5 +10,26 @@ def print_board():
     print(board_items[0] + '|' + board_items[1] + '|' + board_items[2])
 
 
+# ================================
+# Function for selecting character
+def choose_character_option():
+    ch = ''
+
+    while ch not in ['0', 'X']:
+        ch = input('Please enter you desired character (0 or X): ')
+
+        if ch not in ['0', 'X']:
+            print('Please choose right option (0 or X)')
+
+    if ch == 'X':
+        return ('X', '0')
+    else:
+        return ('0', 'X')
+
+
+# ===============================
 # Main operation starts from here
-print_board()
+while game_running:
+    print_board()
+    player1, player2 = choose_character_option()
+    print(f'Your character: {player1}')
